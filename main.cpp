53c00c8b5f0ec525cd6b818de2bfe3c35583bbf3
Prashant <prashant@Prashants-MacBook-Air.local>
2013-09-22 01:11:40 +0200

    some formatting changes in main

--- main.cpp
+++ main.cpp
@@ -15,26 +15,27 @@ using namespace std;
 int main()
 {  
    
-    Core * core = Core::get_instance("/home/yash/PhoneDirectory/Contacts.txt");
+    Core * core = Core::get_instance("./Contacts.txt");
     assert(core!=0);
     core->interface(0,0);
     int option_id=0;
  
     while(option_id != -1){
   
-    	std::cout<<" Possible options are: "<<std::endl;
-    	std::cout<<" LOAD from database : 0 "<< std::endl;
-    	std::cout<<" REFRESH database   : 1" << std::endl;
-    	std::cout<<" SHOW all contacts  : 2 "<< std::endl;
-    	std::cout<<" ADD a contact      : 3 "<< std::endl;
-    	std::cout<<" DEL a contact      : 4 "<<std::endl;
-    	std::cout<<" SORT the contacts  : 5 "<<std::endl;
-    	std::cout<<" SEARCH a contact   : 6 "<<std::endl;
-    	std::cout<<" Get no of contacts : 7 "<<std::endl;
+    	std::cout<< endl << endl << " ** Possible options are: **"<<std::endl;
+    	std::cout<<" LOAD from database : 0,\t";
+    	std::cout<<" REFRESH database   : 1,\t";
+    	std::cout<<" SHOW all contacts  : 2,\t";
+    	std::cout<<" ADD a contact      : 3,\t";
+    	std::cout<<" DEL a contact      : 4\t" << endl;
+    	std::cout<<" SORT the contacts  : 5,\t";
+    	std::cout<<" SEARCH a contact   : 6,\t";
+    	std::cout<<" Get no of contacts : 7,\t";
     	std::cout<<" Exit Application   : -1"<<std::endl;
-   	std::cout<< "Enter the option:";
+   	std::cout<< "\t ** Enter the option ** : ";
     	int option_id;
         std::cin>>option_id;
+	cout << endl << endl;
  
    if((option_id < -1) || (option_id > 7)){
        std::cout<<" Invalid option... " << std::endl;
